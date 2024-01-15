f665a065fe96acc288af9c91515d2cb745d51497
i-m-Paras <awasthiparas6@gmail.com>
2024-01-15 16:13:14 +0530

    week3 04 solved

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,89 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+const zod = require("zod");
+
+const adminSchema = zod.object({
+  username: zod.string().email(),
+  password: zod.string().min(6),
+});
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+
+  const parseData = adminSchema.safeParse({
+    username,
+    password,
+  });
+
+  if (!parseData.success) {
+    res.status(411).json({
+      msg: "invalid credentials!",
+    });
+  } else {
+    await Admin.create({
+      username,
+      password,
+    });
+    res.json({ message: "Admin created successfully" });
+  }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+
+  const { username, password } = req.body;
+
+  const parseData = adminSchema.safeParse({
+    username,
+    password,
+  });
+
+  if (!parseData.success) {
+    res.status(411).json({
+      msg: "invalid credentials!",
+    });
+  } else {
+    const user = await Admin.findOne({ username, password });
+
+    if (user) {
+      const token = jwt.sign({ username }, JWT_SECRET);
+
+      res.json({
+        token,
+      });
+    } else {
+      res.status(411).json({
+        msg: "Incorrect username and pass!",
+      });
+    }
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const data = req.body;
+
+  const user = await Course.create({ ...data });
+
+  res.json({
+    message: "Course created successfully",
+    courseId: user._id,
+  });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const response = await Course.find({});
+
+  res.json({
+    courses: response,
+  });
 });
 
 module.exports = router;
--- week-3/04-mongo-with-jwt-auth/solution/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/solution/routes/user.js
@@ -1,29 +1,95 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
+const { JWT_SECRET } = require("../config");
+const jwt = require("jsonwebtoken");
+const zod = require("zod");
+
+const adminSchema = zod.object({
+  username: zod.string().email(),
+  password: zod.string().min(6),
+});
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  const parseData = adminSchema.safeParse({
+    username,
+    password,
+  });
+
+  if (!parseData.success) {
+    res.status(411).json({
+      msg: "invalid credentials!",
+    });
+  } else {
+    await User.create({
+      username,
+      password,
+    });
+    res.json({ message: "User created successfully" });
+  }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+
+  const { username, password } = req.body;
+
+  const parseData = adminSchema.safeParse({
+    username,
+    password,
+  });
+
+  if (!parseData.success) {
+    res.status(411).json({
+      msg: "invalid credentials!",
+    });
+  } else {
+    const user = await Admin.findOne({ username, password });
+
+    if (user) {
+      const token = jwt.sign({ username }, JWT_SECRET);
+
+      res.json({
+        token,
+      });
+    } else {
+      res.status(411).json({
+        msg: "Incorrect username and pass!",
+      });
+    }
+  }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const response = await Course.find({});
+
+  res.json({
+    courses: response,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
-    const username = req.username;
-    console.log(username);
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const username = req.username;
+  const courseId = req.params.courseId;
 
+  await User.updateOne(
+    { username },
+    {
+      $push: {
+        purchasedCourses: courseId,
+      },
+    }
+  );
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, (req, res) => {
+  // Implement fetching purchased courses logic
 });
 
-module.exports = router
+module.exports = router;
